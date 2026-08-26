dc48d7b28b3692dc0c0da40129912479f90454c5
Wellington Barbosa <huehueb58@gmail.com>
2026-08-26 13:36:33 -0300

    feat: adicionado routes

--- 2026.08.19/projeto-teste/src/app/app.ts
+++ 2026.08.19/projeto-teste/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
