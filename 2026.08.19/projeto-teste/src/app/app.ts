939f72583a7cdb1e5fba46dc727bdccbedc9c75b
Wellington Barbosa <huehueb58@gmail.com>
2026-08-28 15:19:46 -0300

    feat: adicionado componente header

--- 2026.08.19/projeto-teste/src/app/app.ts
+++ 2026.08.19/projeto-teste/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { login, logout, usuarioLogado } from './core/auth';
 import { MatButtonModule } from '@angular/material/button';
+import { Header } from './shared/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, MatButtonModule],
+  imports: [RouterOutlet, RouterLink, MatButtonModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- 2026.08.19/projeto-teste/src/app/shared/layout/header/header.ts
+++ 2026.08.19/projeto-teste/src/app/shared/layout/header/header.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
